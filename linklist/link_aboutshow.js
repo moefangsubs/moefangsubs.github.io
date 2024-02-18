f39aa8823c4b10997c30b77eb0a30f6d7fbdcc6b
moefangsubs <123193875+moefangsubs@users.noreply.github.com>
2024-02-18 19:09:19 +0700

    Delete linklist directory

--- linklist/link_aboutshow.js
+++
@@ -1,7 +0,0 @@
-// link_aboutshow.js
-
-const acaraData = {
-    nogistar: "\"Chou Nogizaka Star Tanjou!\" merupakan season keempat dari Nogizaka Star Tanjou. Kembali bersama gen-5 dan MC komedian Oswald, tidak hanya menyanyikan lagu-lagu populer era Showa dan Heisei, namun juga mereka untuk pertama kalinya menantang diri dalam berakting di sketsa komedi dengan nama segmen Nogista Skits.",
-    nogidougaBurari: "\"Nogizaka Burari\" merupakan konten dari Nogi Douga di mana satu member dari Nogizaka46 diminta untuk merancang \"Jika menghabiskan liburan bersama member, apa yang akan dilakukan, dengan siapa, dan di mana?\" sebagai tema. Mereka akan menghabiskan hari libur sesuai dengan rencana ideal mereka sambil mengambil foto selfie, dan konten ini akan memberikan gambaran tentang pengalaman tersebut.",
-    nogiotameshichuu: "\"Nogizaka Otameshichuu\" merupakan variety show para member Under. Slot acara ini menggantikan variety sebelumnya, Nogi Eigo. Di awal episode hingga episode 10, pembawa acaranya adalah dari gen-2, Terada Ranze. Karena memutuskan lulus, episode setelahnya dibawakan oleh Ito Riria dari gen-3. MC ini didampingi partnernya dari seorang komedian Suzuki Taku. Variety ini akan mencoba berbagai tantangan secara acak."
-};
