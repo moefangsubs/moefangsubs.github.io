7a652e821568542a2a4bc1a93540286667eb468d
moefangsubs <123193875+moefangsubs@users.noreply.github.com>
2024-02-25 23:54:00 +0700

    Create liveperformance.js

---
+++ store/eplist/liveperformance.js
@@ -0,0 +1,7 @@
+document.write(`
+		<div class="scroll-container">
+			<div class="imglist">
+				<div class="episodelist buttonEpsList" data-episode="01"><img onclick="applyEffect(this)" src="https://ik.imagekit.io/moearchive/thumb/other/231219-bokuao-oneman-vol0.jpg"><div class="epsname">231219 僕青</div><span class="epsbadgeNew"></span></div>
+			</div>
+		</div>
+`);
