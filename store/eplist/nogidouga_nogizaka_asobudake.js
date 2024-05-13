c4c35e4a6601432e5b1f2b8a563862a9f231ccbe
Moe Fang <123193875+moefangsubs@users.noreply.github.com>
2024-05-13 19:24:10 +0700

    Update Asobudake

--- store/eplist/nogidouga_nogizaka_asobudake.js
+++ store/eplist/nogidouga_nogizaka_asobudake.js
@@ -1,6 +1,12 @@
 document.write(`
 		<div class="scroll-container">
 			<div class="imglist">
+				<div class="episodelist buttonEpsList" data-episode="50b"><img onclick="applyEffect(this)" src="https://ik.imagekit.io/moearchive/thumb/variety/asobudake50b.jpg"><div class="epsname">Episode 50 後編</div></div>
+				<div class="episodelist buttonEpsList" data-episode="50a"><img onclick="applyEffect(this)" src="https://ik.imagekit.io/moearchive/thumb/variety/asobudake50a.jpg"><div class="epsname">Episode 50 前編</div></div>
+				<div class="episodelist buttonEpsList" data-episode="49"><img onclick="applyEffect(this)" src="https://ik.imagekit.io/moearchive/thumb/variety/asobudake49.jpg"><div class="epsname">Episode 49</div></div>
+				<div class="episodelist buttonEpsList" data-episode="48"><img onclick="applyEffect(this)" src="https://ik.imagekit.io/moearchive/thumb/variety/asobudake48.jpg"><div class="epsname">Episode 48</div></div>
+				<div class="episodelist buttonEpsList" data-episode="47"><img onclick="applyEffect(this)" src="https://ik.imagekit.io/moearchive/thumb/variety/asobudake47.jpg"><div class="epsname">Episode 47</div></div>
+				
 				<div class="episodelist buttonEpsList" data-episode="44b"><img onclick="applyEffect(this)" src="https://ik.imagekit.io/moearchive/thumb/variety/asobudake44b.jpg"><div class="epsname">Episode 44 後編</div></div>
 				<div class="episodelist buttonEpsList" data-episode="44a"><img onclick="applyEffect(this)" src="https://ik.imagekit.io/moearchive/thumb/variety/asobudake44a.jpg"><div class="epsname">Episode 44 前編</div></div>
 				<div class="episodelist buttonEpsList" data-episode="18"><img onclick="applyEffect(this)" src="https://ik.imagekit.io/moearchive/thumb/variety/asobudake18.jpg"><div class="epsname">Episode 18</div></div>
