ba593eceada6e1b9ec6186619e4a4122afc43fbb
Moe Fang <123193875+moefangsubs@users.noreply.github.com>
2024-05-18 19:45:45 +0700

    Add movie Kitagawa Yuri

---
+++ store/eplist/shiawase_nakereba.js
@@ -0,0 +1,7 @@
+document.write(`
+<div class="scroll-container">
+<div class="imglist">
+<div class="episodelist buttonEpsList" data-episode="01"><img onclick="applyEffect(this)" src="https://ik.imagekit.io/moearchive/thumb/docrandom/shiawasenakereba.jpg"><div class="epsname">Movie</div></div>
+</div>
+</div>
+`);
