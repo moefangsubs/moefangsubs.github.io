8e97b8b6c89d0783a4fd7833f181236bfa4e2fa7
Moe Fang <123193875+moefangsubs@users.noreply.github.com>
2024-07-01 01:45:25 +0700

    New look (July 2024)

--- js/formLogin.js
+++ js/formLogin.js
@@ -1,23 +1,42 @@
 // formLogin.js
 
-    function checkPassword() {
-        var password = document.getElementById("password").value;
-        var correctPassword = "Hashimoto_Nanami@2017";
+function checkPassword() {
+    var passwordInput = document.getElementById("password");
+    var password = passwordInput.value;
+    var correctPassword = "Hashimoto_Nanami@2017";
+    var placeholderText = passwordInput.getAttribute('placeholder');
 
-        if (password === correctPassword) {
-            localStorage.setItem("enteredPassword", password);
-            window.location.href = "sitemap.html";
-        } else {
-            var form = document.getElementById("passwordForm");
-            form.reset();
-            form.style.backgroundColor = "red";
-            setTimeout(function() {
-                form.style.backgroundColor = "";
-            }, 2000);
-        }
+    if (password === correctPassword) {
+        localStorage.setItem("enteredPassword", password);
+        window.location.href = "sitemap.html";
+    } else {
+        passwordInput.setAttribute('placeholder', 'WRONG PASSWORD :(');
+        passwordInput.value = "";
+        passwordInput.style.backgroundColor = "white";
+        setTimeout(function() {
+            passwordInput.style.backgroundColor = "";
+            passwordInput.setAttribute('placeholder', placeholderText);
+        }, 2000);
     }
+}
 
-    document.getElementById("passwordForm").addEventListener("submit", function(event) {
-        event.preventDefault();
-        checkPassword();
+
+document.getElementById("passwordForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    checkPassword();
+});
+
+document.getElementById("togglePassword").addEventListener("click", function() {
+    var passwordField = document.getElementById("password");
+    var type = passwordField.getAttribute("type") === "password" ? "text" : "password";
+    passwordField.setAttribute("type", type);
+    this.textContent = type === "password" ? "👁️" : "🙈";
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const ifpernahDiv = document.querySelector('.ifpernah');
+    
+    ifpernahDiv.addEventListener('click', () => {
+        window.location.href = 'sitemap.html';
     });
+});
