3038bd603565577b162f0a7dfa36cecf5fcd3a22
Moe Fang <123193875+moefangsubs@users.noreply.github.com>
2024-09-17 15:57:39 +0700

    Add files via upload

--- js/formLogin.js
+++ js/formLogin.js
@@ -1,6 +1,6 @@
 // formLogin.js
-// document.body.style.backgroundColor = '#1e0e00';
 
+// Fungsi untuk memeriksa password
 function checkPassword() {
     var passwordInput = document.getElementById("password");
     var password = passwordInput.value;
@@ -17,16 +17,32 @@ function checkPassword() {
         setTimeout(function() {
             passwordInput.style.backgroundColor = "";
             passwordInput.setAttribute('placeholder', placeholderText);
-        }, 2000);
+        }, 5000);
     }
 }
 
+// Pengecekan apakah user sudah pernah memasukkan password
+document.addEventListener("DOMContentLoaded", function() {
+    var storedPassword = localStorage.getItem("enteredPassword");
 
-document.getElementById("passwordForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-    checkPassword();
+    // Tampilkan animasi loading selama 5 detik
+    setTimeout(function() {
+        if (storedPassword === "Hashimoto_Nanami@2017") {
+            // Jika user sudah memasukkan password sebelumnya, redirect ke sitemap setelah 3 detik
+            document.getElementById("loading-screen").style.display = "none";
+            document.getElementById("loading-okay").style.display = "block";
+            setTimeout(function() {
+                window.location.href = "sitemap.html";
+            }, 3000);
+        } else {
+            // Jika user belum pernah memasukkan password, tampilkan form login
+            document.getElementById("loading-screen").style.display = "none";
+            document.getElementById("main-container").style.display = "flex";
+        }
+    }, 5000);
 });
 
+// Fungsi untuk menampilkan atau menyembunyikan password
 document.getElementById("togglePassword").addEventListener("click", function() {
     var passwordField = document.getElementById("password");
     var type = passwordField.getAttribute("type") === "password" ? "text" : "password";
@@ -34,10 +50,8 @@ document.getElementById("togglePassword").addEventListener("click", function() {
     this.textContent = type === "password" ? "👁️" : "🙈";
 });
 
-document.addEventListener('DOMContentLoaded', (event) => {
-    const ifpernahDiv = document.querySelector('.ifpernah');
-    
-    ifpernahDiv.addEventListener('click', () => {
-        window.location.href = 'sitemap.html';
-    });
+// Event listener untuk submit form
+document.getElementById("passwordForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    checkPassword();
 });
