e0a67c45a2284ba663343b46327b149961445da5
Moe Fang <123193875+moefangsubs@users.noreply.github.com>
2024-11-27 20:16:16 +0700

    Update MV

--- pages/js/script_mv.js
+++ pages/js/script_mv.js
@@ -5,7 +5,7 @@ const nextBtn = document.querySelector('.next-btn');
 let currentIndex = 0;
 
 nextBtn.addEventListener('click', () => {
-  if (currentIndex < 41) {
+  if (currentIndex < 42) {
     currentIndex++;
   } else {
     currentIndex = 0;
@@ -17,7 +17,7 @@ prevBtn.addEventListener('click', () => {
   if (currentIndex > 0) {
     currentIndex--;
   } else {
-    currentIndex = 41;
+    currentIndex = 42;
   }
   updateSlider();
 });
