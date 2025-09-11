07539554c894576745501cb40a09644ad7e01bc9
moefangsubs <moefangsubs@gmail.com>
2025-09-11 15:17:52 +0700

    Update

--- js/footer.js
+++ js/footer.js
@@ -104,7 +104,7 @@ document.addEventListener('DOMContentLoaded', function() {
         <div class="warning-footer">
 			<div class="about">
 				<img class="foot-label" src="../sprite/main.svg"/>
-				<span>Data Update : 8 September 2025 23:30 WIB</span>
+				<span>Data Update : 11 September 2025 15:15 WIB</span>
 				<span>Data bisa saja ada kesalahan input.</span>
 				<span>Jika menemukannya dan/atau ingin ada masukan, mohon hubungi admin via DM Instagram</span>
 			</div>
