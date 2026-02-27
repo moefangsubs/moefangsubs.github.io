0c902c12561bf15800c35e1ce084305eca6ffffa
moefangsubs <moefangsubs@gmail.com>
2026-02-27 16:19:09 +0700

    Update 260227

--- js/footer.js
+++ js/footer.js
@@ -135,7 +135,7 @@ document.addEventListener('DOMContentLoaded', function() {
 			</div>
 			<div class="about">
 				<img class="foot-label" src="../sprite/main.svg"/>
-				<span>Data Update : 24 Februari 2026 18:32 WIB</span>
+				<span>Data Update : 27 Februari 2026 16:10 WIB</span>
 				<span>Data bisa saja ada kesalahan input.</span>
 				<span>Jika menemukannya dan/atau ingin ada masukan, mohon hubungi admin via DM Instagram</span>
 			</div>
